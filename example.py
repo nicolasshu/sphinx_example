03af9ab18ac3121480ccf48400f612ff91d7f2b1
Nick Shu <nicolas.shu@ll.mit.edu>
2024-08-26 12:55:49 -0400

    Add a subtract function to example.py

--- example.py
+++ example.py
@@ -17,3 +17,23 @@ def add(a,b):
     """
 
     return a+b
+
+def subtract(a,b):
+    """
+
+    Subtract two numbers
+
+    Parameters
+    ----------
+    a : int
+        A number
+    b : float
+        Another number
+
+    Examples
+    --------
+    subtract(1,2)
+
+    """
+
+    return a-b
