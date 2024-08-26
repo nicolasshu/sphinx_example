8efce8680349cfadef7af9910becf221dc6a3c41
Nick Shu <nicolas.shu@ll.mit.edu>
2024-08-26 10:47:09 -0400

    Create a dummy package

---
+++ yeti/mato.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class Mato:
+    def __init__(self, file: str):
+        """
+        Initialize the Mato
+
+        Parameters
+        ----------
+        file : str
+            File to set
+        """
+        self.file = file
+    def print(self):
+        """
+        Print the Mato file
+
+        """
+        print(self.file)
+    def add1(self, num):
+        """
+
+        Add 1 to the num
+
+        Parameters
+        ----------
+        num : int
+            Number to add
+        """
+        return num + 1
