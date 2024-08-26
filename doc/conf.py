86c68bb05294d2fb759cfd4fc28139654cf0c521
Nick Shu <nicolas.shu@ll.mit.edu>
2024-08-26 11:25:57 -0400

    Change theme back to RTD

--- doc/conf.py
+++ doc/conf.py
@@ -27,6 +27,6 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'press'
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'press'
 html_static_path = ['_static']
